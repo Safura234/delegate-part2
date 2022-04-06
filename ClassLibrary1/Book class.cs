018ddeda6a7753e701736f5e661875a1eba08369
Safura234 <safuramg@code.edu.az>
2022-04-06 16:33:19 +0400

    https://github.com/Safura234/delegate-part2.git

---
+++ ClassLibrary1/Book class.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    internal class Book_class
+    {
+    }
+}
---
+++ assignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        interface IEntity
+        {
+            int Id { get; set; }    
+
+}
+    }
+}
